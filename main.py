068e154d677f1b1c68e77e7f9d73573fe9491d5f
Likith-23 <likithsairajpignos@gmail.com>
2024-10-09 20:28:57 +0200

    intial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#program to show working of different bit wise operators
+num1 = 99
+num2 = 98
+#using an operator
+print("num1 & num2 = ", num1 & num2)
+
+#using or operator
+print("\n num1 | num2 =", num1 | num2)
+
+#using not operator
+print("\n ~ num1 =", ~ num1)
+print("\n ~ num2 =", ~ num2)
+
+#using xor operator
+print("\n num1 ^ num2 =", num1 ^ num2)
+
+#using right shift operator on num1
+print("\n num1 >> 1 = ", num1 >> 1)
+
+#using left shift operator on num1
+print("\n num1 << 1 = ", num1 << 1)
+
+#using right shift operator on num1
+print("\n num2 >> 1 = ", num2 >> 1)
+
+#using left shift operator on num1
+print("\n num2 << 1 = ", num2 << 1)
